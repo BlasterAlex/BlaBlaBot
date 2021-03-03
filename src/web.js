fd41891dffb1e88d3c2c723b25d19961f37b359f
Alexander <pichulev23071999@gmail.com>
2021-03-03 20:50:04 +0400

    Disable ping heroku

--- src/web.js
+++ src/web.js
@@ -19,21 +19,21 @@ var server = app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
 // Держать бота активным
 if (process.env.HEROKU_URL) {
 
-  const interval = 15 * 60 * 1000; // interval in milliseconds - 15 mins
-  (function wake(url) {
-    var handler;
-    try {
-      handler = setInterval(() => {
-        fetch(url)
-          .then(res => console.log('Ping heroku: ' + (res.ok ? 'OK' : 'NOT OK') + ` status: ${res.status}`))
-          .catch(err => console.error(`Ping heroku error: ${err}`));
-      }, interval);
-    } catch (err) {
-      console.error('Ping heroku error: retrying...');
-      clearInterval(handler);
-      return setTimeout(() => wake(url), 10000);
-    }
-  })(process.env.HEROKU_URL);
+  // const interval = 15 * 60 * 1000; // interval in milliseconds - 15 mins
+  // (function wake(url) {
+  //   var handler;
+  //   try {
+  //     handler = setInterval(() => {
+  //       fetch(url)
+  //         .then(res => console.log('Ping heroku: ' + (res.ok ? 'OK' : 'NOT OK') + ` status: ${res.status}`))
+  //         .catch(err => console.error(`Ping heroku error: ${err}`));
+  //     }, interval);
+  //   } catch (err) {
+  //     console.error('Ping heroku error: retrying...');
+  //     clearInterval(handler);
+  //     return setTimeout(() => wake(url), 10000);
+  //   }
+  // })(process.env.HEROKU_URL);
 
 }
 
