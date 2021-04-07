e8512a7f6ac889d20db88c90f268b853c7ac9635
Alexander <pichulev23071999@gmail.com>
2021-04-07 12:45:09 +0400

    Add UseBasicParsing parameter for running IE in batch

--- scripts/devMode.ps1
+++ scripts/devMode.ps1
@@ -1,4 +1,4 @@
 # Переключение бота в режим разработки
 get-content .\config\private.json | Where-Object { $_ -match '^\s*\"TELEGRAM_TOKEN\":\s*\"(.*)\"' } | out-null
 $Token = $Matches.1
-Invoke-WebRequest https://api.telegram.org/bot$Token/setWebhook?url=
+Invoke-WebRequest https://api.telegram.org/bot$Token/setWebhook?url= -UseBasicParsing
